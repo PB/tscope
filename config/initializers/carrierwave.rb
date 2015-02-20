0d1d65bd83bdb74eca02b0cd306814911c398f78
Piotr B <b.piotr@gmail.com>
2015-02-20 16:23:07 +0100

    s3 error

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',                        # required
       :aws_access_key_id      => ENV["aws_access_key_id"],     # required
-      :aws_secret_access_key  => ENV["aws_secret_access_key"]  # required
-      #:region                 => 'Ireland',                    # optional, defaults to 'us-east-1'
+      :aws_secret_access_key  => ENV["aws_secret_access_key"],  # required
+      :region                 => 'eu-west-1',                    # optional, defaults to 'us-east-1'
       #:host                   => 's3.example.com',             # optional, defaults to nil
       #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
   }
